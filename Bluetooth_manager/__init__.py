73941ad3728c4851f3bb8830354f5fd41770835f
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-04-13 22:26:32 -0400

    Update __init__.py

--- Bluetooth_manager/__init__.py
+++ Bluetooth_manager/__init__.py
@@ -4,6 +4,10 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+# Load requirements from requirements.txt file
+with open('requirements.txt', 'r', encoding='utf-8') as fh:
+    requirements = fh.readlines()
+
 setup(
     name='bluetooth_manager',
     version='0.1.0',
@@ -14,17 +18,13 @@ setup(
     author='Ant O, Greene',
     author_email='anthonygreene2007@gmail.com',
     url='https://github.com/LoQiseaking69/BT_manager',
-    install_requires=[
-        'PyYAML',  # Assuming PyYAML is a dependency for reading config files
-        'subprocess32;python_version<"3.5"',  # For subprocess compatibility in Python < 3.5
-    ],
+    install_requires=requirements,
     python_requires='>=3.6',
     classifiers=[
-        # Classifiers help users find your project by categorizing it.
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -32,4 +32,5 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
+    license='AGPLv3',
 )
