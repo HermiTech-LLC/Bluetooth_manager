32f2adb20dc8e0402a3df8231dd6de71234592f2
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-04-13 22:31:15 -0400

    Update __init__.py

--- Bluetooth_manager/__init__.py
+++ Bluetooth_manager/__init__.py
@@ -1,36 +1,19 @@
-from setuptools import setup, find_packages
+# Import key classes and functions from your modules
+from .bluetooth_manager import BluetoothManager
+from .device_discovery import discover_devices, connect_device
+from .config_loader import load_config
 
-# Improved handling of README file for long description
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+# If you have constants or utilities that should be accessible
+# from the package, you can also import them here:
+from .constants import DEFAULT_TIMEOUT, SUPPORTED_DEVICES
 
-# Load requirements from requirements.txt file
-with open('requirements.txt', 'r', encoding='utf-8') as fh:
-    requirements = fh.readlines()
-
-setup(
-    name='bluetooth_manager',
-    version='0.1.0',
-    packages=find_packages(),
-    description='A package to manage Bluetooth devices using bluetoothctl',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Ant O, Greene',
-    author_email='anthonygreene2007@gmail.com',
-    url='https://github.com/LoQiseaking69/BT_manager',
-    install_requires=requirements,
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-    license='AGPLv3',
-)
+# Optionally, you can define an __all__ list which specifies what
+# symbols will be exported when 'from package import *' is used.
+__all__ = [
+    'BluetoothManager',
+    'discover_devices',
+    'connect_device',
+    'load_config',
+    'DEFAULT_TIMEOUT',
+    'SUPPORTED_DEVICES'
+]
