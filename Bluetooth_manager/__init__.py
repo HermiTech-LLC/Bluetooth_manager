b1546b1d2d6ca266900cf05607aec6c566e0e350
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-04-14 12:46:35 -0400

    Update __init__.py

--- Bluetooth_manager/__init__.py
+++ Bluetooth_manager/__init__.py
@@ -1,3 +1,11 @@
-from .manager import BluetoothManager, BluetoothManagerError, load_config
+"""
+Bluetooth Manager Package.
 
-__all__ = ['BluetoothManager', 'BluetoothManagerError', 'load_config']
+This package provides classes and utilities to manage Bluetooth devices using
+the bluetoothctl tool from the command line.
+"""
+
+__version__ = '0.1.0'
+from .manager import BluetoothManager, BluetoothManagerError
+
+__all__ = ['BluetoothManager', 'BluetoothManagerError']
