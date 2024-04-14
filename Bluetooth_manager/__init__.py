5a29a52e9cf867147eace71a3e8bc433342806cc
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-04-14 02:26:23 -0400

    Update __init__.py

--- Bluetooth_manager/__init__.py
+++ Bluetooth_manager/__init__.py
@@ -1,19 +1,3 @@
-# Import key classes and functions from your modules
-from .bluetooth_manager import BluetoothManager
-from .device_discovery import discover_devices, connect_device
-from .config_loader import load_config
+from .manager import BluetoothManager, BluetoothManagerError, load_config
 
-# If you have constants or utilities that should be accessible
-# from the package, you can also import them here:
-from .constants import DEFAULT_TIMEOUT, SUPPORTED_DEVICES
-
-# Optionally, you can define an __all__ list which specifies what
-# symbols will be exported when 'from package import *' is used.
-__all__ = [
-    'BluetoothManager',
-    'discover_devices',
-    'connect_device',
-    'load_config',
-    'DEFAULT_TIMEOUT',
-    'SUPPORTED_DEVICES'
-]
+__all__ = ['BluetoothManager', 'BluetoothManagerError', 'load_config']
